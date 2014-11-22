bc1d5016c65d475b6de6c1e657f6ad828d8b6add
Diana Sighinolfi <diana.sighinolfi@gmail.com>
2014-11-22 15:20:11 +0100

    Versione 3.9
    Tolto le stampe a cazzo

--- src/Main.java
+++ src/Main.java
@@ -24,8 +24,6 @@ public class Main {
 	private static LinkedList<String> allAuthors = new LinkedList<String>();
 	
 	public static void main(String[] args) {
-		System.out.println("PESCIOLINO\n");
-		System.out.println("sono la Diana\n");
 		window = new pagina();
 		inizializeBook();
 		window.getRdbtnTutto().doClick();
